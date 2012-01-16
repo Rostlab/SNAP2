818f94ae955f6aa5be1f6cd4b1850e7c0b129700
Maximilian Hecht <hecht@rostlab.org>
2012-01-16 15:04:37 +0000

    some corrections
    git-svn-id: svn+ssh://i12k-biolab01.informatik.tu-muenchen.de/mnt/project/subversion/snap2/trunk@32 6e6737b7-5487-4a1d-9bcb-0c9e4bc3b8b4

--- lib/RG/Snap2/Extract.pm
+++ lib/RG/Snap2/Extract.pm
@@ -15,80 +15,80 @@ sub all{
     my ($workdir,$debug)=@_;
     my @data;
     my $seqlength=length($main::sequence); 
-    my $pssm=new Pssmparser("$workdir/$main::name.blastPsiMat");
-    my $psic=new Psicparser("$workdir/$main::name.psic");
-    #my $isis=new Isisparser("$workdir/$main::name.isis");
-    #my $disis=new Disisparser("$workdir/$main::name.disis");
-    #my $md=new Mdparser("$workdir/$main::name.mdisorder");
-    my $prof=new Profparser("$workdir/$main::name.reprof");
-    my $swiss=new Swissparser("$workdir/$main::name.blastswiss",$main::swiss_dat,$main::db_swiss);
-    my $profbval=new Profbvalparser("$workdir/$main::name.profbval");
-    my $oriprof=new Profparser("$workdir/$main::name.reprof_ORI");
-    #my $snap=new Snapparser("$workdir/$main::name.quick");
+    my $pssm=new RG::Snap2::Pssmparser("$workdir/$main::name.blastPsiMat");
+    my $psic=new RG::Snap2::Psicparser("$workdir/$main::name.psic");
+    #my $isis=new RG::Snap2::Isisparser("$workdir/$main::name.isis");
+    #my $disis=new RG::Snap2::Disisparser("$workdir/$main::name.disis");
+    #my $md=new RG::Snap2::Mdparser("$workdir/$main::name.mdisorder");
+    my $prof=new RG::Snap2::Profparser("$workdir/$main::name.reprof");
+    my $swiss=new RG::Snap2::Swissparser("$workdir/$main::name.blastswiss",$main::swiss_dat,$main::db_swiss);
+    my $profbval=new RG::Snap2::Profbvalparser("$workdir/$main::name.profbval");
+    my $oriprof=new RG::Snap2::Profparser("$workdir/$main::name.reprof_ORI");
+    #my $snap=new RG::Snap2::Snapparser("$workdir/$main::name.quick");
 
 
 
     #Global Features, only extracted once
-    my @aacomp=Features::aa_comp($seqlength,\@main::sequence_array,$debug);
-    my @acccomp=Features::acc_comp($seqlength,\$prof,$debug);
-    my @seccomp=Features::sec_comp($seqlength,\$prof,$debug);
-    my @protlength=Features::protein_length($seqlength,$debug);
+    my @aacomp=RG::Snap2::Features::aa_comp($seqlength,\@main::sequence_array,$debug);
+    my @acccomp=RG::Snap2::Features::acc_comp($seqlength,\$prof,$debug);
+    my @seccomp=RG::Snap2::Features::sec_comp($seqlength,\$prof,$debug);
+    my @protlength=RG::Snap2::Features::protein_length($seqlength,$debug);
 
     #Loop over all mutants and extract all features
     foreach my $i (0..@main::todo-1){
         my @features;
         #split mutation A36D into wildtype, position, mutant: 'A', '36', 'D'
-        my ($wt,$pos,$mut)=Features::wt_pos_mut($main::todo[$i],$debug);
+        my ($wt,$pos,$mut)=RG::Snap2::Features::wt_pos_mut($main::todo[$i],$debug);
 
         #Global Features
         push @features,@aacomp,@acccomp,@seccomp,@protlength;
 
         #Sequence based features
-        push @features,Features::blastPssm($seqlength,$pos,\$pssm,5,$debug);
-        push @features,Features::blastPerc($seqlength,$pos,\$pssm,1,$debug);
-        push @features,Features::psic($seqlength,$pos,\$psic,13,$debug);
-        push @features,Features::sequenceprofile(\@main::sequence_array,$seqlength,$pos,9,$debug);
-        #push @features,Features::isis($seqlength,$pos,\$isis,17,$debug);
-        #push @features,Features::disis($seqlength,$pos,\$disis,5,$debug);
-        #push @features,Features::md($seqlength,$pos,\$md,9,$debug);
-        #push @features,Features::sec_bin($seqlength,$pos,\$prof,13,$debug);
-        #push @features,Features::sec_raw($seqlength,$pos,\$prof,9,$debug);
-        push @features,Features::acc_rel($seqlength,$pos,\$prof,5,$debug);
-        #push @features,Features::acc_bin($seqlength,$pos,\$prof,5,$debug);
-        #push @features,Features::indices($seqlength,$pos,\@main::sequence_array,17,'charge',$debug);
-        #push @features,Features::indices($seqlength,$pos,\@main::sequence_array,17,'hyd',$debug);
-        #push @features,Features::indices($seqlength,$pos,\@main::sequence_array,17,'vol',$debug);
-        push @features,Features::profbval($seqlength,$pos,\$profbval,5,$debug);
+        push @features,RG::Snap2::Features::blastPssm($seqlength,$pos,\$pssm,5,$debug);
+        push @features,RG::Snap2::Features::blastPerc($seqlength,$pos,\$pssm,1,$debug);
+        push @features,RG::Snap2::Features::psic($seqlength,$pos,\$psic,13,$debug);
+        push @features,RG::Snap2::Features::sequenceprofile(\@main::sequence_array,$seqlength,$pos,9,$debug);
+        #push @features,RG::Snap2::Features::isis($seqlength,$pos,\$isis,17,$debug);
+        #push @features,RG::Snap2::Features::disis($seqlength,$pos,\$disis,5,$debug);
+        #push @features,RG::Snap2::Features::md($seqlength,$pos,\$md,9,$debug);
+        #push @features,RG::Snap2::Features::sec_bin($seqlength,$pos,\$prof,13,$debug);
+        #push @features,RG::Snap2::Features::sec_raw($seqlength,$pos,\$prof,9,$debug);
+        push @features,RG::Snap2::Features::acc_rel($seqlength,$pos,\$prof,5,$debug);
+        #push @features,RG::Snap2::Features::acc_bin($seqlength,$pos,\$prof,5,$debug);
+        #push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,17,'charge',$debug);
+        #push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,17,'hyd',$debug);
+        #push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,17,'vol',$debug);
+        push @features,RG::Snap2::Features::profbval($seqlength,$pos,\$profbval,5,$debug);
         
         #Difference features
-        push @features,Features::pssmDiff($wt,$pos,$mut,\$pssm,$debug);
-        push @features,Features::percDiff($wt,$pos,$mut,\$pssm,$debug);
-        push @features,Features::psicDiff($wt,$pos,$mut,\$psic,$debug);
-        #push @features,Features::indexDiff($wt,$mut,'vol',$debug);
-        #push @features,Features::indexDiff($wt,$mut,'charge',$debug);
-        #push @features,Features::profDiff($wt,$pos,$mut,"$workdir/$main::name.reprof_$main::todo[$i]",\$oriprof,$debug);
+        push @features,RG::Snap2::Features::pssmDiff($wt,$pos,$mut,\$pssm,$debug);
+        push @features,RG::Snap2::Features::percDiff($wt,$pos,$mut,\$pssm,$debug);
+        push @features,RG::Snap2::Features::psicDiff($wt,$pos,$mut,\$psic,$debug);
+        #push @features,RG::Snap2::Features::indexDiff($wt,$mut,'vol',$debug);
+        #push @features,RG::Snap2::Features::indexDiff($wt,$mut,'charge',$debug);
+        #push @features,RG::Snap2::Features::profDiff($wt,$pos,$mut,"$workdir/$main::name.reprof_$main::todo[$i]",\$oriprof,$debug);
 
         #Mutation based features
-        push @features,Features::swiss($wt,$pos,$mut,\$swiss,$main::phat_matrix,$debug);
-        push @features,Features::sift("$workdir/$main::name.SIFTprediction",$main::todo[$i],$debug);
-        push @features,Features::residue_representation($mut,$debug);
+        push @features,RG::Snap2::Features::swiss($wt,$pos,$mut,\$swiss,$main::phat_matrix,$debug);
+        push @features,RG::Snap2::Features::sift("$workdir/$main::name.SIFTprediction",$main::todo[$i],$debug);
+        push @features,RG::Snap2::Features::residue_representation($mut,$debug);
 
         #Additional indices from quicksnap
-        #push @features,Features::indices($seqlength,$pos,\@main::sequence_array,17,'VINM940103',$debug);
-        #push @features,Features::indices($seqlength,$pos,\@main::sequence_array,9,'BLAM930101',$debug);
-        #push @features,Features::indices($seqlength,$pos,\@main::sequence_array,5,'DAYM780201',$debug);
-        #push @features,Features::indices($seqlength,$pos,\@main::sequence_array,5,'QIAN880123',$debug);
-        #push @features,Features::indices($seqlength,$pos,\@main::sequence_array,13,'KLEP840101',$debug);
-        #push @features,Features::indexDiff($wt,$mut,'SNEP660101',$debug);
-        #push @features,Features::indexDiff($wt,$mut,'RICJ880113',$debug);
-        #push @features,Features::indexDiff($wt,$mut,'KLEP840101',$debug);
-        #push @features,Features::indexDiff($wt,$mut,'VINM940103',$debug);
-        #push @features,Features::potentials($seqlength,$pos,\@main::sequence_array,9,$wt,$debug);
-        #push @features,Features::potentials($seqlength,$pos,\@main::sequence_array,9,$mut,$debug);
-        push @features,Features::potentialDiff($seqlength,$pos,\@main::sequence_array,9,$wt,$mut,$debug);
-        push @features,Features::potentialProfileDiff($wt,$mut,$debug);
-        #push @features,Features::qsnap_pred(\$snap,$main::todo[$i],$debug);
-        #push @features,Features::qsnap_avg(\$snap,$pos,5,$seqlength,$debug);
+        #push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,17,'VINM940103',$debug);
+        #push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,9,'BLAM930101',$debug);
+        #push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,5,'DAYM780201',$debug);
+        #push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,5,'QIAN880123',$debug);
+        #push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,13,'KLEP840101',$debug);
+        #push @features,RG::Snap2::Features::indexDiff($wt,$mut,'SNEP660101',$debug);
+        #push @features,RG::Snap2::Features::indexDiff($wt,$mut,'RICJ880113',$debug);
+        #push @features,RG::Snap2::Features::indexDiff($wt,$mut,'KLEP840101',$debug);
+        #push @features,RG::Snap2::Features::indexDiff($wt,$mut,'VINM940103',$debug);
+        #push @features,RG::Snap2::Features::potentials($seqlength,$pos,\@main::sequence_array,9,$wt,$debug);
+        #push @features,RG::Snap2::Features::potentials($seqlength,$pos,\@main::sequence_array,9,$mut,$debug);
+        push @features,RG::Snap2::Features::potentialDiff($seqlength,$pos,\@main::sequence_array,9,$wt,$mut,$debug);
+        push @features,RG::Snap2::Features::potentialProfileDiff($wt,$mut,$debug);
+        #push @features,RG::Snap2::Features::qsnap_pred(\$snap,$main::todo[$i],$debug);
+        #push @features,RG::Snap2::Features::qsnap_avg(\$snap,$pos,5,$seqlength,$debug);
         
         #append to data array
         push (@data,\@features);
@@ -104,38 +104,38 @@ sub quick{
     my $seqlength=length($main::sequence);
 
     #Global Features, only extracted once
-    my @aacomp=Features::aa_comp($seqlength,\@main::sequence_array,$debug);
-    my @protlength=Features::protein_length($seqlength,$debug);
+    my @aacomp=RG::Snap2::Features::aa_comp($seqlength,\@main::sequence_array,$debug);
+    my @protlength=RG::Snap2::Features::protein_length($seqlength,$debug);
 
     #Loop over all mutants and extract all features
     foreach my $i (0..@main::todo-1){
         my @features;
         
         #split mutation A36D into wildtype, position, mutant: 'A', '36', 'D'
-        my ($wt,$pos,$mut)=Features::wt_pos_mut($main::todo[$i],$debug);
+        my ($wt,$pos,$mut)=RG::Snap2::Features::wt_pos_mut($main::todo[$i],$debug);
         
         #Global
         push @features,@aacomp,@protlength;
 
         #Sequence window
-        push @features,Features::indices($seqlength,$pos,\@main::sequence_array,17,'VINM940103',$debug);
-        push @features,Features::indices($seqlength,$pos,\@main::sequence_array,9,'BLAM930101',$debug);
-        push @features,Features::indices($seqlength,$pos,\@main::sequence_array,5,'DAYM780201',$debug);
-        push @features,Features::indices($seqlength,$pos,\@main::sequence_array,5,'QIAN880123',$debug);
-        push @features,Features::indices($seqlength,$pos,\@main::sequence_array,13,'KLEP840101',$debug);
-        push @features,Features::sequenceprofile(\@main::sequence_array,$seqlength,$pos,9,$debug);
-        push @features,Features::potentials($seqlength,$pos,\@main::sequence_array,9,$wt,$debug);
-        push @features,Features::potentials($seqlength,$pos,\@main::sequence_array,9,$mut,$debug);
+        push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,17,'VINM940103',$debug);
+        push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,9,'BLAM930101',$debug);
+        push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,5,'DAYM780201',$debug);
+        push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,5,'QIAN880123',$debug);
+        push @features,RG::Snap2::Features::indices($seqlength,$pos,\@main::sequence_array,13,'KLEP840101',$debug);
+        push @features,RG::Snap2::Features::sequenceprofile(\@main::sequence_array,$seqlength,$pos,9,$debug);
+        push @features,RG::Snap2::Features::potentials($seqlength,$pos,\@main::sequence_array,9,$wt,$debug);
+        push @features,RG::Snap2::Features::potentials($seqlength,$pos,\@main::sequence_array,9,$mut,$debug);
 
         #Difference
-        push @features,Features::indexDiff($wt,$mut,'SNEP660101',$debug);
-        push @features,Features::indexDiff($wt,$mut,'RICJ880113',$debug);
-        push @features,Features::indexDiff($wt,$mut,'vol',$debug);
-        push @features,Features::indexDiff($wt,$mut,'KLEP840101',$debug);
-        push @features,Features::indexDiff($wt,$mut,'VINM940103',$debug);
-        push @features,Features::residue_representation($mut,$debug);
-        push @features,Features::potentialProfileDiff($wt,$mut,$debug);
-        push @features,Features::potentialDiff($seqlength,$pos,\@main::sequence_array,9,$wt,$mut,$debug);
+        push @features,RG::Snap2::Features::indexDiff($wt,$mut,'SNEP660101',$debug);
+        push @features,RG::Snap2::Features::indexDiff($wt,$mut,'RICJ880113',$debug);
+        push @features,RG::Snap2::Features::indexDiff($wt,$mut,'vol',$debug);
+        push @features,RG::Snap2::Features::indexDiff($wt,$mut,'KLEP840101',$debug);
+        push @features,RG::Snap2::Features::indexDiff($wt,$mut,'VINM940103',$debug);
+        push @features,RG::Snap2::Features::residue_representation($mut,$debug);
+        push @features,RG::Snap2::Features::potentialProfileDiff($wt,$mut,$debug);
+        push @features,RG::Snap2::Features::potentialDiff($seqlength,$pos,\@main::sequence_array,9,$wt,$mut,$debug);
 
         #append to data array
         push (@data,\@features);
